a766f8a90e41e286c96ec5977897912994898e9e
Scott-MASE <a00325403@student.tus.ie>
2025-01-06 13:07:31 +0000

    springboot demo

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
+
+@RestController
+class HelloController {
+	
+	@GetMapping("/")
+	public String hi() {
+		return "Hello, world!";
+	}
+
+	@GetMapping("/greet")
+	public String greet(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return "Hello, " + name + "!";
+	}
+
+	@GetMapping("/status")
+	public String status() {
+		return "Application is running smoothly!";
+	}
+}
