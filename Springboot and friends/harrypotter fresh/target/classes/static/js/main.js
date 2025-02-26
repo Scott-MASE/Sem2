d471f2fe6669275b713257a12635cbf8fa9041c6
Scott-MASE <a00325403@student.tus.ie>
2025-02-26 19:04:19 +0000

    huh

--- Springboot and friends/harrypotter fresh/target/classes/static/js/main.js
+++ Springboot and friends/harrypotter fresh/target/classes/static/js/main.js
@@ -1,66 +1,18 @@
-
-var RootURl = "http://localhost:8081/books"
+var rootUrl = "http://localhost:8081/books";
 
 var findAll = function(){
 	$.ajax({
 		type: "GET",
-		data: "json",
-		url: RootURl,
+		dataType: "json",
+		url: rootUrl,
 		success: renderList,
-		error: function(xhr,status,error){
-			console.log(error);
-		}
-		
-	});
-	
-};
-
-var renderList = function(data){
-	$(".details").remove();
-	$.each(data, function(index, book){
-		let img = '"images/' + book.image +'"';
-		let htmlStr = '<div id=' + book.id + ' class="details">';
-		htmlStr+='<img src=' + img + 'height="150">';
-		htmlStr+='<h1>'+book.title+'</h1>';
-		htmlStr+='<h2>By ' + book.author + '</h2>';
-		htmlStr+= '<h2>Illustrated by ' + book.illustrated + '</h2>';
-		htmlStr+='<input id="' + book.id + '" class="infoButton" type="button" value="More Info">';
-		htmlStr+='</div>';
-		$("#bookList").append(htmlStr);
-	});
-};
-
-var findById = function(id){
-	$.ajax({
-		type:"GET",
-		data:"json",
-		url: RootURl + '/' + id,
-		success: moreInfo,	
 		error: function(error){
 			console.log(error);
 		}
 	});
 };
 
-var moreInfo = function(){
-	let modal = '#detailsModal';
-	
-	modal.find('.modal-title').text(book.title);
-	modal.find('#pic').attr('src', 'images/' + book.imageModal);
-	
-	let rrp = book.rrp;
-	let onprice = book.online;
-	let savings = rrp - onprice;
-	
-	modal.find('#rrp').text(rrp)
-	
-	modal.find('#rrp').text('')
-	
-	
-}
-
-$(document).ready(function(){
-	findAll();
-	
-});
-
+var renderList = function(data){
+	$(".details").remove()
+	let img = "/images/"+book.image;	
+};
